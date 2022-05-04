2e749cdff682b845dade87bd7cda4bb53f448d07
Yee Choong <yeechoong97@gmail.com>
2022-05-04 13:35:34 +0800

    Completed play pause function

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Text, TouchableWithoutFeedback, View } from 'react-native'
 import Video from 'react-native-video'
 import styles from './style'
 
 const Post = () => {
+
+    const [paused, setPaused] = useState(false);
+
+    const onPlayPausePress = () => {
+        setPaused(!paused);
+    }
+
     return (
         <View style={styles.container}>
-            <Video
-                source={{ uri: "https://res.cloudinary.com/fyp202105/video/upload/v1651641928/pexels-taro-5595352_qa4pja.mp4" }}
-                style={styles.video}
-                resizeMode={'cover'}
-            />
+            <TouchableWithoutFeedback onPress={onPlayPausePress}>
+                <Video
+                    source={{ uri: "https://res.cloudinary.com/fyp202105/video/upload/v1651641928/pexels-taro-5595352_qa4pja.mp4" }}
+                    style={styles.video}
+                    resizeMode={'cover'}
+                    repeat={true}
+                    paused={paused}
+                />
+            </TouchableWithoutFeedback>
         </View>
     )
 }
