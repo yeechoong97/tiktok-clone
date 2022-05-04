b2446c6a07167c1aef923c1a724c481120afc8f0
Yee Choong <yeechoong97@gmail.com>
2022-05-04 12:01:20 +0800

    Added amplify and partial home component

---
+++ src/screens/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Homescreen</Text>
+        </View>
+    )
+}
+
+export default Home
