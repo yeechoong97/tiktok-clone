5da2162661c75d77451d881d858ad6a5a3232b81
Yee Choong <yeechoong97@gmail.com>
2022-05-04 16:22:02 +0800

    Completed the post component

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
-import { Text, TouchableWithoutFeedback, View } from 'react-native'
+import { Image, Text, TouchableWithoutFeedback, View } from 'react-native'
 import Video from 'react-native-video'
 import styles from './style'
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 const Post = () => {
 
@@ -14,13 +18,45 @@ const Post = () => {
     return (
         <View style={styles.container}>
             <TouchableWithoutFeedback onPress={onPlayPausePress}>
-                <Video
-                    source={{ uri: "https://res.cloudinary.com/fyp202105/video/upload/v1651641928/pexels-taro-5595352_qa4pja.mp4" }}
-                    style={styles.video}
-                    resizeMode={'cover'}
-                    repeat={true}
-                    paused={paused}
-                />
+                <View>
+                    <Video
+                        source={{ uri: "https://res.cloudinary.com/fyp202105/video/upload/v1651641928/pexels-taro-5595352_qa4pja.mp4" }}
+                        style={styles.video}
+                        resizeMode={'cover'}
+                        repeat={true}
+                        paused={paused}
+                    />
+                    <View style={styles.uiContainer}>
+
+                        <View style={styles.rightContainer}>
+                            <Image style={styles.profilePicture} source={{ uri: 'https://res.cloudinary.com/fyp202105/image/upload/v1614037856/Avatar/Avatar-14.png' }} />
+                            <View style={styles.iconContainer}>
+                                <AntDesign name={'heart'} size={35} color="white" />
+                                <Text style={styles.statsLabel}>Like</Text>
+                            </View>
+                            <View style={styles.iconContainer}>
+                                <FontAwesome name={'commenting'} size={35} color="white" />
+                                <Text style={styles.statsLabel}>Like</Text>
+                            </View>
+                            <View style={styles.iconContainer}>
+                                <Fontisto name={'share-a'} size={35} color="white" />
+                                <Text style={styles.statsLabel}>Like</Text>
+                            </View>
+                        </View>
+
+                        <View style={styles.bottomContainer}>
+                            <View>
+                                <Text style={styles.handle}>Bottom</Text>
+                                <Text style={styles.description}>Description</Text>
+                                <View style={styles.songRow}>
+                                    <Entypo name={"beamed-note"} size={24} color="white" />
+                                    <Text style={styles.songName}>Counting Starts</Text>
+                                </View>
+                            </View>
+                            <Image style={styles.songImage} source={{ uri: "https://upload.wikimedia.org/wikipedia/en/2/21/%3F_XXXTENTACION_Cover.png" }} />
+                        </View>
+                    </View>
+                </View>
             </TouchableWithoutFeedback>
         </View>
     )
--- src/components/Post/style.js
+++ src/components/Post/style.js
@@ -11,7 +11,70 @@ const styles = StyleSheet.create({
     container: {
         width: '100%',
         height: Dimensions.get('window').height
+    },
+    uiContainer: {
+        height: '100%',
+        paddingBottom: 24,
+        justifyContent: 'flex-end',
+    },
+    bottomContainer: {
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end'
+    },
+    handle: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '700',
+        marginBottom: 10,
+    },
+    description: {
+        color: '#C4C4C4',
+        fontSize: 14,
+        fontWeight: '300',
+        marginBottom: 10,
+    },
+    songRow: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    songName: {
+        color: '#fff',
+        fontSize: 14,
+        marginLeft: 5
+    },
+    songImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 5,
+        borderColor: '#4c4c4c',
+    },
+    // Right Container
+    rightContainer: {
+        alignSelf: 'flex-end',
+        height: 300,
+        justifyContent: "space-between"
+    },
+    profilePicture: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 2,
+        borderColor: 'white',
+        marginRight: 10,
+    },
+    statsLabel: {
+        color: '#fff',
+        marginTop: 5,
+    },
+    iconContainer: {
+        alignItems: 'center',
+        fontSize: 16,
+        fontWeight: '500'
     }
+
 });
 
 export default styles;
